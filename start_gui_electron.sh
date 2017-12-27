6a0964f7869a2e8bf37569a809891d706635ff84
kanasimi <kanasimi@gmail.com>
2017-12-28 05:46:51 +0800

    fix #1: release 1.0

--- start_gui_electron.sh
+++ start_gui_electron.sh
@@ -3,11 +3,20 @@
 
 # npm install electron-builder
 # for electron-builder:
-# npm install nsis
+# NG: npm install nsis
+#
+# %HOMEPATH%\node_modules\.bin\electron-builder --linux
+# node_modules\.bin\electron-builder --win --ia32 --x64
+# node_modules\.bin\electron-builder --wl
+# node_modules\.bin\electron-builder --linux
+#
+# https://www.electron.build/configuration/configuration
+# https://newsn.net/say/electron-builder-basic.html
+# https://electron.org.cn/builder//configuration/configuration.html#configuration
+# https://www.electron.build/configuration/nsis
 
 
 # npm install electron-packager
-
 
 node_modules/.bin/electron . || ~/node_modules/.bin/electron .
 
