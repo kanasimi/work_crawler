6c4e99df40964d1bc64e9e5557a94f472f52d0d0
kanasimi <kanasimi@gmail.com>
2018-07-13 00:04:37 +0800

    fix #20, #24: manhuagui 不允許過於頻繁的 access，會直接 ban IP。

--- comic.cmn-Hans-CN/manhuagui.js
+++ comic.cmn-Hans-CN/manhuagui.js
@@ -38,7 +38,8 @@ crawler = new CeL.work_crawler({
 
 	// 2018/4: manhuagui 不允許過於頻繁的 access，會直接 ban IP。
 	// 當網站不允許太過頻繁的訪問/access時，可以設定下載之前的等待時間(ms)。
-	chapter_time_interval : 1 * 1000,
+	// 2018/7/12 22:29:18 9s: NG
+	chapter_time_interval : 10 * 1000,
 
 	// 2018/3/3 已經不再有常常出現錯誤的情況。
 	// allow .jpg without EOI mark.
