a3ceae71eb8f16acd47e47885d450cbde426ea22
kanasimi <kanasimi@gmail.com>
2018-07-27 19:42:56 +0800

    fix #37: add 批量下載 LINE WEBTOON 漫畫 的工具。 Download NAVER WEBTOON comics.

---
+++ comic.en-US/webtoon.js
@@ -0,0 +1,23 @@
+﻿/**
+ * 批量下載 LINE WEBTOON 漫畫 的工具。 Download NAVER WEBTOON comics.
+ */
+
+'use strict';
+
+require('../work_crawler_loder.js');
+
+// ----------------------------------------------------------------------------
+
+CeL.run('application.net.work_crawler.webtoon');
+
+// ----------------------------------------------------------------------------
+
+var crawler = CeL.webtoon({
+	language_code : 'en'
+});
+
+// ----------------------------------------------------------------------------
+
+// CeL.set_debug(3);
+
+start_crawler(crawler, typeof module === 'object' && module);
