f7361762e694518bc5dd77a473936850f8c6dec4
kanasimi <kanasimi@gmail.com>
2018-07-27 19:33:24 +0800

    fix #37: add 批量下載 XOY 漫畫 的工具。 Download XOY comics. (comic.ja-JP)

---
+++ comic.cmn-Hans-CN/XOY.js
@@ -0,0 +1,24 @@
+﻿/**
+ * 批量下載 XOY 漫畫 的工具。 Download XOY comics. (comic.ja-JP)
+ */
+
+'use strict';
+
+require('../work_crawler_loder.js');
+
+// ----------------------------------------------------------------------------
+
+CeL.run('application.net.work_crawler.webtoon');
+
+// ----------------------------------------------------------------------------
+
+var crawler = CeL.webtoon({
+	base_URL : 'https://xoy.webtoons.com/',
+	language_code : 'ja'
+});
+
+// ----------------------------------------------------------------------------
+
+// CeL.set_debug(3);
+
+start_crawler(crawler, typeof module === 'object' && module);
