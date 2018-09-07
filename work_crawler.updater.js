a9e918a460235fd33391495682c946b80ce91644
kanasimi <kanasimi@gmail.com>
2018-09-07 18:23:28 +0800

    fix 下載/更新 Colorless echo JavaScript kit 組件 - 2

--- work_crawler.updater.js
+++ work_crawler.updater.js
@@ -68,8 +68,9 @@ function update_components(update_script_name) {
 
 	show_info('下載/更新 Colorless echo JavaScript kit 組件...');
 	child_process.execSync('node '
-			+ (executing_at_tool_directory ? update_script_name + ' .' : '../'
-					+ update_script_name), {
+			+ (executing_at_tool_directory ? update_script_name
+			// @see default_repository_path @ update_script_name
+			+ ' kanasimi/CeJS .' : '../' + update_script_name), {
 		stdio : 'inherit'
 	});
 
