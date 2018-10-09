c03e78f857c11350fd8fbc7fd6368c563ea37e07
kanasimi <kanasimi@gmail.com>
2018-10-10 07:29:08 +0800

    fix #94: 投稿新星專區作品

--- comic.cmn-Hans-CN/webtoon.js
+++ comic.cmn-Hans-CN/webtoon.js
@@ -15,7 +15,30 @@ CeL.run('application.net.work_crawler.webtoon');
 
 var crawler = CeL.webtoon({
 	// https://www.webtoons.com/zh-hant/
-	language_code : 'zh-hant'
+	language_code : 'zh-hant',
+
+	// 2018/10: 投稿新星專區 用預設方法搜尋不到，得要用網頁的方法。
+	search_URL : 'search?keyword=',
+	parse_search_result : function(html, get_label) {
+		// console.log(html);
+		// <h3 class="search_result">投稿新星專區作品 (1個結果)</h3>
+		html = html.between('"search_result"',
+		// <div class="ranking_lst search">
+		'"ranking_lst search"');
+
+		var id_list = [], id_data = [];
+		html.each_between('<li>', '</li>', function(token) {
+			// console.log(token);
+			var matched = token
+			// e.g., <a href="/challenge/episodeList?titleNo=211344"
+			.match(/(?:\/([a-z]+)\/)?episodeList\?titleNo=(\d+)/);
+			id_list.push(matched[1] ? matched[1] + '_' + matched[2]
+					: +matched[2]);
+			id_data.push(get_label(token.between('<p class="subj">', '</p>')));
+		});
+
+		return [ id_list, id_data ];
+	}
 });
 
 // ----------------------------------------------------------------------------
