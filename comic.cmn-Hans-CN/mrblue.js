a3318a08dc001114817e034c5d9e7c4ac18447af
kanasimi <kanasimi@gmail.com>
2018-10-28 10:47:22 +0800

    fix #105: 僅僅利用可預測的圖片網址序列去下載漫畫作品的範例。

--- comic.cmn-Hans-CN/mrblue.js
+++ comic.cmn-Hans-CN/mrblue.js
@@ -1,5 +1,7 @@
 ﻿/**
- * 批量下載 漫画 的工具。 Download comics.
+ * 批量下載 미스터블루 (Mr.Blue) 漫画 的工具。 Download mrblue comics.
+ * 
+ * 本檔案為僅僅利用可預測的圖片網址序列去下載漫畫作品的範例。
  */
 
 'use strict';
@@ -12,7 +14,15 @@ var crawler = new CeL.work_crawler({
 	// 所有的子檔案要修訂註解說明時，應該都要順便更改在CeL.application.net.comic中Comic_site.prototype內的母comments，並以其為主體。
 
 	one_by_one : true,
+	skip_error : true,
+	preserve_bad_image : false,
+	MAX_ERROR_RETRY : 2,
+
+	// http://www.mrblue.com/webtoon/all
 	base_URL : 'http://comics.mrblue.com/',
+
+	skip_get_work_page : true,
+	skip_get_chapter_page : true,
 
 	// 取得作品的章節資料。 get_work_data()
 	work_URL : function(work_id) {
@@ -23,6 +33,7 @@ var crawler = new CeL.work_crawler({
 	},
 	get_chapter_list : function(work_data, html, get_label) {
 		work_data.chapter_list = [ this.work_URL(work_data.id) ];
+		// console.log(work_data);
 	},
 
 	parse_chapter_data : function(html, work_data, get_label, chapter_NO) {
@@ -35,13 +46,15 @@ var crawler = new CeL.work_crawler({
 			} ]
 		};
 
+		// console.log(chapter_data);
 		return chapter_data;
 	},
 
-	after_get_image : function(image_list, work_data, chapter_NO) {
-		var image_data = image_list[image_list.index];
-		if (!image_data.has_error) {
-			// 下載本章節下一幅圖片。
+	after_get_images : function(image_list, work_data, chapter_NO) {
+		// console.log(image_list);
+		var latest_image_data = image_list[image_list.index];
+		if (!latest_image_data.has_error) {
+			// CeL.debug(work_data.id + ': 本章節上一張圖片下載成功。下載本章節下一幅圖片。');
 			image_list.push({
 				url : this.work_URL(work_data.id) + chapter_NO + '/'
 						+ (image_list.length + 1).pad(3) + '.jpg'
@@ -50,12 +63,14 @@ var crawler = new CeL.work_crawler({
 		}
 
 		if (image_list.length === 1) {
-			// 第一張圖就失敗了。結束下載本作品。
+			// CeL.debug(work_data.id + ': 第一張圖就失敗了。結束下載本作品。');
 			return;
 		}
 
-		// 下載下一個章節的圖片。
-		work_data.chapter_list(this.work_URL(work_data.id));
+		// CeL.debug(work_data.id + ': 本章節上一張圖片下載失敗。下載下一個章節的圖片。');
+		work_data.chapter_list.push(this.work_URL(work_data.id));
+		// 動態增加章節，必須手動增加章節數量。
+		work_data.chapter_count++;
 	},
 	dynamical_count_images : true
 });
