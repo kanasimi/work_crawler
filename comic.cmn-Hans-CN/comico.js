0b6eb073c94b6465d057f295364c8f48cc7f102e
kanasimi <kanasimi@gmail.com>
2018-12-10 17:16:58 +0800

    fix #129: comico 改版

--- comic.cmn-Hans-CN/comico.js
+++ comic.cmn-Hans-CN/comico.js
@@ -15,11 +15,12 @@ CeL.run('application.net.work_crawler.comico');
 
 // CeL.set_debug(3);
 
+// crawler(configuration, callback, initializer)
 CeL.comico({
 	base_URL : 'http://www.comico.com.tw/',
 
-	search_head_token : '<li class="list-article02__item">',
-	PATTERN_search : /<a href="[^<>"]*?titleNo=(\d+)"[\s\S]*? alt="([^"]+)"/,
+	// search_head_token : '<li class="list-article02__item">',
+	// PATTERN_search : /<a href="[^<>"]*?titleNo=(\d+)"[\s\S]*? alt="([^"]+)"/,
 
 	// 取得作品的章節資料。 get_work_data()
 	work_URL : function(work_id) {
