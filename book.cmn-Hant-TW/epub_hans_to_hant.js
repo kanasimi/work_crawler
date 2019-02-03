575d4322cc4e6df6a3957a9e6d86bb86d262ef6d
kanasimi <kanasimi@gmail.com>
2019-02-04 07:05:48 +0800

    fix function for_text_file(path, fso_status, is_directory)

--- book.cmn-Hant-TW/epub_hans_to_hant.js
+++ book.cmn-Hant-TW/epub_hans_to_hant.js
@@ -18,7 +18,8 @@ CeL.run([ 'application.storage.archive', 'application.storage.EPUB',
 function initialization() {
 	var epub_file_path = process.argv[2];
 	if (!epub_file_path) {
-		var main_script = process.mainModule && process.mainModule.filename.match(/[^\\\/]+$/)[0];
+		var main_script = process.mainModule
+				&& process.mainModule.filename.match(/[^\\\/]+$/)[0];
 		CeL.log('Convert epub: hans to hant. 將 epub 簡體中文電子書轉換為繁體中文的工具。\n\n'
 				+ 'Usage:\n	node ' + main_script + ' "epub.epub"');
 		return;
@@ -38,6 +39,7 @@ function initialization() {
 
 	CeL.debug('Extract epub files: ' + epub_file_path);
 	archive_file.ebook_file_list = [];
+	process.stdout.write('Extracting ebook ' + epub_file_path + ' ...\r');
 	archive_file.extract({
 		output : epub_directory
 	}, function convert_files(output) {
@@ -45,6 +47,7 @@ function initialization() {
 		// console.log(CeL.traverse_file_system + '');
 		CeL.traverse_file_system(epub_directory, for_text_file
 				.bind(archive_file));
+
 		archive_file.ebook_file_list = archive_file.ebook_file_list
 		// assert: path.startsWith(epub_directory+CeL.env.path_separator)
 		.map(function(path) {
@@ -56,11 +59,13 @@ function initialization() {
 				' (cmn-Hant-TW)$1');
 		CeL.remove_file(converted_epub_file);
 
-		CeL.debug('Pack epub file: ' + converted_epub_file);
+		process.stdout.write('Packing epub file: ' + converted_epub_file
+				+ ' ...\r');
 		// 打包 epub。結果會存放到與 epub_file_path 相同的目錄。
 		(new CeL.EPUB(epub_directory)).archive(converted_epub_file, true,
 				archive_file.ebook_file_list);
 
+		// TODO: if error occurred, do not remove directory.
 		CeL.debug('Remove directory: ' + epub_directory);
 		CeL.remove_directory(epub_directory, true);
 	});
@@ -74,10 +79,12 @@ function for_text_file(path, fso_status, is_directory) {
 	if (!/\.(?:[sx]?html?|xml|te?xt|ncx|opf)$/i.test(path))
 		return;
 
-	CeL.log('Convert to hant: ' + path);
+	process.stdout.write('Convert to hant: ' + path + ' ...\r');
 	var contents = CeL.get_file(path);
 	contents = CeL.CN_to_TW(contents)
 	// TODO: 把半形標點符號轉換為全形標點符號
-	.replace(/["'](?:zh-(?:cmn-)?|cmn-)?(?:Hans-)?CN["']/ig, 'zh-cmn-Hant-TW');
+	.replace(/["'](?:zh-(?:cmn-)?|cmn-)?(?:Hans-)?CN["']/ig,
+	// "zh-TW"
+	'"zh-cmn-Hant-TW"');
 	CeL.write_file(path, contents);
-};
+}
