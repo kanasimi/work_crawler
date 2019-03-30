f406ff1c513dd3ef46b0d4ac1efc731c2b534f00
kanasimi <kanasimi@gmail.com>
2019-03-30 09:22:42 +0800

    fix #258: 2019/3 pixivコミック 大改版 改用API 幾乎重寫

--- archive/pixivcomic.2018-201903.js
+++ archive/pixivcomic.2018-201903.js
@@ -1,7 +1,5 @@
 ﻿/**
  * 批量下載 pixivコミック(ぴくしぶこみっく) 的工具。 Download pixiv comic.
- * 
- * @since 2019/3 pixivコミック 大改版
  */
 
 'use strict';
@@ -10,95 +8,117 @@ require('../work_crawler_loder.js');
 
 // ----------------------------------------------------------------------------
 
-var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
+var crawler = new CeL.work_crawler({
 	// 所有的子檔案要修訂註解說明時，應該都要順便更改在CeL.application.net.comic中Comic_site.prototype內的母comments，並以其為主體。
-
-	// e.g., 785 働かないふたり 第45話
-	skip_error : true,
 
 	// 日本的線上漫畫網站習慣刪掉舊章節，因此每一次都必須從頭檢查。
 	recheck : true,
 
 	// one_by_one : true,
-	base_URL : base_URL,
+	base_URL : 'https://comic.pixiv.net/',
 
 	// 解析 作品名稱 → 作品id get_work()
 	store : false,
 	search_URL : function(work_title) {
-		return [ 'api/app/' + (this.store ? 'store' : 'works') + '/search/v2/'
-		//
-		+ encodeURIComponent(work_title), null, {
-			headers : {
-				// 'X-Requested-With' 可以用來判斷客戶端的請求是Ajax請求還是其他請求。
-				'X-Requested-With' : base_URL
-			}
-		} ];
+		return (this.store ? 'store/search/' : 'search/')
+		// this.store: ストア作品
+		+ encodeURIComponent(work_title);
 	},
 	parse_search_result : function(html, get_label) {
-		// console.log(html);
+		var id_list = [], id_data = [], text = html.between(
+				'<div class="layout-searches">', '</section>');
+		if (text) {
+			html.each_between('<div class="search-item">', '</a></div>',
+			//
+			function(text) {
+				var url = text.match(/ href="([^<>"]+)"/),
+				//
+				title = get_label(text.between('<p class="search-title">',
+						'</p>'));
+				id_list.push(url[1].match(/\/(\d+)$/)[1]);
+				id_data.push(title);
+			});
+		} else {
+			// ストア作品
+			text = html.between('<div class="work-list flexwrap">');
+			html.each_between('<div class="items"', '</h5></div>',
+			//
+			function(text) {
+				var title = get_label(text.between('<h4 class="work-title">',
+						'</h4>')), url = title.match(/ href="([^<>"]+)"/);
+				id_list.push('store_' + url[1].match(/\/([a-z\d]+)$/)[1]);
+				id_data.push(title);
+			});
+		}
 
-		// {"data":{"official_works":[{"id":5447,"name":"姫乃ちゃんに恋はまだ早い","author":"ゆずチリ","description":"相川姫乃、小学４年生。同級生の逢司くんに、恋心をうまく伝えらなくて――…。\r\u003cbr\u003eちょっとおませなラブコメディ開幕♪
-		// \r\u003cbr\u003eコミックス第１巻は２月９日発売!!","like_count":11299,"image":{"main":"https://public-img-comic.pximg.net/c!/w=200,f=webp%3Ajpeg/images/work_main/5447.jpg?20190206095841","thumbnail":"https://public-img-comic.pximg.net/c!/q=90,f=webp%3Ajpeg/images/work_thumbnail/5447.jpg?20190206095841","main_big":"https://public-img-comic.pximg.net/images/work_main/5447.jpg?20190206095841"},"is_new_work":false,"pixiv_comic_badge":false,"last_story_read_start_at":1553828400000}]}}
-		html = JSON.parse(html).data.official_works;
-		return [ html, html ];
+		return [ id_list, id_data ];
 	},
-	id_of_search_result : function(search_result) {
-		return this.store ? 'store_' + search_result.key : search_result.id;
-	},
-	title_of_search_result : 'name',
 
 	// 取得作品的章節資料。 get_work_data()
 	work_URL : function(work_id) {
-		var matched = typeof work_id === 'string'
-				&& work_id.match(/^store_([a-z\d]+)$/);
-		return matched ? 'shop/products/' + matched[1] : [
-				'api/app/works/v3/' + work_id, null, {
-					headers : {
-						// 'X-Requested-With' 可以用來判斷客戶端的請求是Ajax請求還是其他請求。
-						'X-Requested-With' : base_URL + 'works/' + work_id
-					}
-				} ];
+		var matched = work_id.match(/^store_([a-z\d]+)$/);
+		return matched ? 'store/products/' + matched[1] : 'works/' + work_id;
 	},
 	parse_work_data : function(html, get_label, extract_work_data) {
-		html = JSON.parse(html);
-		// console.log(html);
-		var work_data = html.data;
-		html = work_data.official_work;
-
-		Object.assign(work_data, {
+		var work_data = {
 			// 必要屬性：須配合網站平台更改。
-			title : html[this.title_of_search_result],
 
 			// 選擇性屬性：須配合網站平台更改。
-			author : html.author,
-			category : html.categories.map(function(category) {
-				return category.name;
+			author : get_label(html.between('<h2 class="author">', '</h2>')),
+			category : html.all_between(' class="category"', '</div>').map(
+			// カテゴリ e.g., https://comic.pixiv.net/works/3202
+			function(token) {
+				return get_label(token.between('>'));
 			}),
-			tags : html.tags.map(function(tag) {
-				return tag.name;
-			}),
+			tags : html.between('<ul class="work-tags">', '</ul>').between(
+					null, {
+						tail : '</li>'
+					}).split('</li>').map(get_label),
+			description : get_label(html.between(
+					'<p class="description-content">', '</p>')),
+			last_update : html.between('<section class="episodes">',
+					'</section>').between(
+					'<div class="episode__read-start-at">', '</div>').replace(
+					/更新日:/, '').trim()
+		};
 
-			// 因為沒有明確記載作品是否完結，一年沒更新就不再檢查。
-			recheck_days : 400
-		});
+		extract_work_data(work_data, html);
+
+		// 因為沒有明確記載作品是否完結，一年沒更新就不再檢查。
+		work_data.recheck_days = 400;
 
 		// console.log(work_data);
 		return work_data;
 	},
 	get_chapter_list : function(work_data, html, get_label) {
-		work_data.chapter_list = work_data.official_work.stories.map(
-		//
-		function(chapter_data) {
-			if (!chapter_data.story)
-				return;
-			return {
-				id : chapter_data.story.id,
-				title : chapter_data.story.short_name,
-				url : 'viewer/stories/' + chapter_data.story.id
+		var matched, PATTERN_chapter = /<a id="(\d+)"([\s\S]+?)<\/a>/g;
+		// https://comic.pixiv.net/works/3858 https://comic.pixiv.net/works/3731
+		html = html.between('<section class="episodes">', '</section>');
+
+		work_data.chapter_list = [];
+		while (matched = PATTERN_chapter.exec(html)) {
+			var text = matched[2];
+			var chapter_data = {
+				id : matched[1],
+				title : get_label(text.between('<div class="episode__number">',
+						'</div>')),
+				url : 'viewer/stories/' + matched[1],
+				date : text.between('<div class="episode__read-start-at">',
+						'</div>').replace(/更新日:/, '').trim()
 			};
-		}).filter(function(chapter_data) {
-			return !!chapter_data;
-		}).reverse();
+			matched = text.match(/<img [^<>]+?data-src="([^<>"]+)"/);
+			if (matched) {
+				chapter_data.cover_url = matched[1];
+			}
+			text = get_label(text.between('<div class="episode__title">',
+					'</div>'));
+			if (text) {
+				chapter_data.title += (chapter_data.title ? ' ' : '') + text;
+			}
+
+			work_data.chapter_list.push(chapter_data);
+		}
+		work_data.chapter_list.reverse();
 
 		// 因為中間的章節可能已經被下架，因此依章節標題來定章節編號。
 		this.set_chapter_NO_via_title(work_data);
@@ -111,9 +131,7 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 	// 必須自行保證執行 callback()，不丟出異常、中斷。
 	: function(XMLHttp, work_data, callback, chapter_NO) {
 		var _this = this, html = XMLHttp.responseText, url = html.between(
-		// <meta name="viewer-api-url"
-		// content="/api/v1/viewer/stories/JXXtyieE41/50811.json">
-		'<meta name="viewer-api-url" content="', '"');
+				'<meta name="viewer-api-url" content="', '"');
 		work_data.token_options = {
 			headers : {
 				// work_data['csrf-token']
@@ -134,18 +152,9 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 		this.get_URL(html.between('<meta name="token-api-url" content="',
 		// /api/v1/viewer/token/d220bbe0ed815ceea5fd0308021fff9f.json
 		'"'), function(XMLHttp) {
-			try {
-				html = JSON.parse(XMLHttp.responseText);
-				if (html.error)
-					throw html.error;
-			} catch (e) {
-				if (_this.skip_error)
-					_this.onwarning(e);
-				else
-					_this.onerror(e);
-				callback();
-				return;
-			}
+			html = JSON.parse(XMLHttp.responseText);
+			if (html.error)
+				throw html.error;
 			var chapter_data = work_data.chapter_list[chapter_NO - 1];
 			chapter_data.token_data = html.data;
 			_this.get_URL('/api/v1/viewer/stories/'
@@ -155,17 +164,9 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 		}, {}, work_data.token_options);
 	},
 	parse_chapter_data : function(html, work_data, get_label, chapter_NO) {
-		try {
-			html = JSON.parse(html);
-			if (html.error)
-				throw html.error;
-		} catch (e) {
-			if (this.skip_error)
-				this.onwarning(e);
-			else
-				this.onerror(e);
-			return;
-		}
+		html = JSON.parse(html);
+		if (html.error)
+			throw html.error;
 
 		var chapter_data = work_data.chapter_list[chapter_NO - 1];
 		// 避免覆寫 chapter_data.title
@@ -192,8 +193,6 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 	finish_up : function(work_data) {
 		// 不記錄 token。
 		delete work_data['csrf-token'];
-		delete work_data['csrf-param'];
-		this.save_work_data(work_data);
 	}
 });
 
@@ -201,6 +200,4 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 
 // CeL.set_debug(3);
 
-// 缺少這個會從章節頁面跳回作品頁面。
-crawler.get_URL_options.cookie = 'open_work_page=yes';
 start_crawler(crawler, typeof module === 'object' && module);
