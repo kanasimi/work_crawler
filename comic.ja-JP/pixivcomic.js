cbb9efbd20eb7af627b7b6a72cd5f9d27afa7750
kanasimi <kanasimi@gmail.com>
2019-05-12 05:35:46 +0800

    fix #284: 名稱顯示成兩行問題。

--- comic.ja-JP/pixivcomic.js
+++ comic.ja-JP/pixivcomic.js
@@ -94,7 +94,10 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 				return;
 			return {
 				id : story.id,
-				title : story.name || story.short_name,
+				// 名稱顯示成兩行。
+				// e.g., https://comic.pixiv.net/works/4252
+				// .short_name="第13話", .name="（４）"
+				title : story.short_name + (story.name || ''),
 				limited : !chapter_data.readable && chapter_data.message,
 				url : 'viewer/stories/' + story.id
 			};
