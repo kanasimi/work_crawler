9efca59d479049318f0a05ae856ff35c3a9bf58c
kanasimi <kanasimi@gmail.com>
2019-06-27 21:11:04 +0800

    fix #323: 修正圖片結尾非正規格式之情況。

--- comic.cmn-Hans-CN/nokiacn.js
+++ comic.cmn-Hans-CN/nokiacn.js
@@ -20,7 +20,24 @@ var crawler = CeL.qTcms2017({
 
 	base_URL : 'http://www.nokiacn.net/',
 
-	image_base_url : 'http://n.aiwenwo.net:55888'
+	image_base_url : 'http://n.aiwenwo.net:55888',
+
+	image_preprocessor : function(contents, image_data) {
+		var index = contents.length - 1;
+
+		if (contents[index] !== 0) {
+			return;
+		}
+
+		// 修正圖片結尾非正規格式之情況。
+		// e.g., http://www.nokiacn.net/yinv/baozhuzheshigeyijinyueye/
+		// http://n.aiwenwo.net:55888/upload2/1774/2018/03-17/20180317232906_4691cbjowu29a_small.jpeg
+
+		while (contents[--index] === 0)
+			;
+
+		return contents.slice(0, index + 1);
+	}
 });
 
 // ----------------------------------------------------------------------------
