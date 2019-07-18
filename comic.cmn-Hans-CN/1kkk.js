0f3320190aed313cea93b4f26f7a6d727c7b1e2c
kanasimi <kanasimi@gmail.com>
2019-07-19 05:52:33 +0800

    fix #338: add 批量下載 极速漫画 漫画人 的工具。 Download 1kkk comics.

---
+++ comic.cmn-Hans-CN/1kkk.js
@@ -0,0 +1,23 @@
+﻿/**
+ * 批量下載 极速漫画 漫画人 的工具。 Download 1kkk comics.
+ */
+
+'use strict';
+
+require('../work_crawler_loader.js');
+
+// ----------------------------------------------------------------------------
+
+CeL.run('application.net.work_crawler.dm5');
+
+// ----------------------------------------------------------------------------
+
+var crawler = CeL.dm5({
+	base_URL : 'http://www.1kkk.com/'
+});
+
+// ----------------------------------------------------------------------------
+
+// CeL.set_debug(3);
+
+start_crawler(crawler, typeof module === 'object' && module);
