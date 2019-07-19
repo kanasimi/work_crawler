365627d41debdf7419971175f44d287cb957ce01
kanasimi <kanasimi@gmail.com>
2019-07-20 07:07:42 +0800

    add 批量下載 欢乐漫画网/多多漫画 的工具。 Download hlgoo/duoduomh comics.

---
+++ comic.cmn-Hans-CN/duoduomh.js
@@ -0,0 +1,26 @@
+﻿/**
+ * 批量下載 欢乐漫画网/多多漫画 的工具。 Download hlgoo/duoduomh comics.
+ */
+
+'use strict';
+
+require('../work_crawler_loader.js');
+
+// ----------------------------------------------------------------------------
+
+CeL.run('application.net.work_crawler.SinMH');
+
+// ----------------------------------------------------------------------------
+
+var crawler = CeL.SinMH({
+	base_URL : 'https://www.hlgoo.cn/',
+
+	search_URL : 'API',
+	id_of_search_result : 'slug'
+});
+
+// ----------------------------------------------------------------------------
+
+// CeL.set_debug(3);
+
+start_crawler(crawler, typeof module === 'object' && module);
