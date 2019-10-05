e688d9b773809c9f8971ca6eb96920a13f8a5ca6
kanasimi <kanasimi@gmail.com>
2019-10-06 06:05:39 +0800

    fix #371: dm5.js 2019/10 改版

--- comic.cmn-Hans-CN/dm5.js
+++ comic.cmn-Hans-CN/dm5.js
@@ -18,7 +18,9 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 var crawler = CeL.dm5({
 	one_by_one : true,
-	base_URL : 'https://www.dm5.com/'
+	// 2019/9: https://www.dm5.com/
+	// 2019/10: http://cnc.dm5.com/
+	base_URL : 'http://cnc.dm5.com/'
 });
 
 // ----------------------------------------------------------------------------
