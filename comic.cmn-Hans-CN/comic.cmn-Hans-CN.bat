6264bcdc7f9813107ec097fa73d67d501890807d
kanasimi <kanasimi@gmail.com>
2019-10-29 06:23:52 +0800

    + archive_old_works

--- comic.cmn-Hans-CN/comic.cmn-Hans-CN.bat
+++ comic.cmn-Hans-CN/comic.cmn-Hans-CN.bat
@@ -6,6 +6,6 @@ REM cd/d D:\USB\cgi-bin\program\comic
 
 REM CD "%~n0"
 REM It's often too slow, so trying to execute parallelly.
-FORFILES /M *.js /C "cmd.exe /c IF @isdir==FALSE START node @file l=@fname.txt || PAUSE"
+FORFILES /M *.js /C "cmd.exe /c IF @isdir==FALSE START node @file l=@fname.txt archive_old_works || PAUSE"
 
 REM CD ..
--- novel.cmn-Hans-CN/novel.cmn-Hans-CN.bat
+++ novel.cmn-Hans-CN/novel.cmn-Hans-CN.bat
@@ -5,6 +5,7 @@ REM cd/d D:\USB\cgi-bin\program\comic
 
 REM CD "%~n0"
 REM It's often too slow, so trying to execute parallelly.
+REM + modify_work_list_when_archive_old_works
 FORFILES /M *.js /C "cmd.exe /c IF @isdir==FALSE START node @file l=list.txt || PAUSE"
 
 chcp 950
