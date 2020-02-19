92756801c622b625386aaaee3cfa793ab8708086
kanasimi <kanasimi@gmail.com>
2020-02-20 07:07:29 +0800

    fix #400: 撲飛漫畫無法下載

--- comic.cmn-Hans-CN/pufei.js
+++ comic.cmn-Hans-CN/pufei.js
@@ -16,9 +16,14 @@ var crawler = CeL.qTcms2014({
 	// 圖像檔案下載失敗處理方式：忽略/跳過圖像錯誤。當404圖像不存在、檔案過小，或是被偵測出非圖像(如不具有EOI)時，依舊強制儲存檔案。default:false
 	skip_error : true,
 
+	// 2020/2/17 http://www.pufei8.com/
 	base_URL : 'http://www.pufei.net/',
 
 	postfix_image_url : function(url) {
+		// 2020/2/17 @see function loadview() @
+		// http://www.pufei.net/skin/2014mh/view.js
+		return 'http://res.img.fffimage.com/' + url;
+
 		// @see function loadview() @ /skin/2014mh/view.js
 		return 'http://res.img.pufei.net/' + url;
 	}
