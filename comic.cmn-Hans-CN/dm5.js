4fd51e5a387f4232f36a0c62a966f30264ba88c4
kanasimi <kanasimi@gmail.com>
2020-02-25 17:45:41 +0800

    fix #401: + extract_work_id

--- comic.cmn-Hans-CN/dm5.js
+++ comic.cmn-Hans-CN/dm5.js
@@ -14,13 +14,22 @@ CeL.run('application.net.work_crawler.sites.dm5');
 
 // https://stackoverflow.com/questions/31673587/error-unable-to-verify-the-first-certificate-in-nodejs
 // fix Error: unable to verify the first certificate
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 var crawler = CeL.dm5({
 	one_by_one : true,
 	// 2019/9: https://www.dm5.com/
 	// 2019/10: 可以使用 http://cnc.dm5.com/
-	base_URL : 'https://www.dm5.com/'
+	// 2020/2: http://www.dm5.com/
+	base_URL : 'http://www.dm5.com/',
+	extract_work_id : function(work_information) {
+		// /^manhua-[a-z\-\d]+$/;
+		// e.g., http://www.dm5.com/manhua-1122/
+		// http://www.dm5.com/manhua--c-94-okazu/
+		return /^manhua-[a-z\-\d]+$/.test(work_information)
+		//
+		&& work_information;
+	}
 });
 
 // ----------------------------------------------------------------------------
