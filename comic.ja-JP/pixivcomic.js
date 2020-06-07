190329199274d0d60ed621fb24b7c1c4cc6f6083
kanasimi <kanasimi@gmail.com>
2020-06-07 17:45:30 +0800

    fix #428: pixiv改版

--- comic.ja-JP/pixivcomic.js
+++ comic.ja-JP/pixivcomic.js
@@ -111,6 +111,7 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 		// 因為中間的章節可能已經被下架，因此依章節標題來定章節編號。
 		this.set_chapter_NO_via_title(work_data);
 
+		// console.log(work_data.chapter_list);
 		// console.log(work_data.official_work.stories);
 	},
 
@@ -118,10 +119,8 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 	// 執行在解析章節資料 process_chapter_data() 之前的作業 (async)。
 	// 必須自行保證執行 callback()，不丟出異常、中斷。
 	: function(XMLHttp, work_data, callback, chapter_NO) {
-		var _this = this, html = XMLHttp.responseText, url = html.between(
-		// <meta name="viewer-api-url"
-		// content="/api/v1/viewer/stories/JXXtyieE41/50811.json">
-		'<meta name="viewer-api-url" content="', '"');
+		// console.log(XMLHttp);
+		var html = XMLHttp.responseText;
 		work_data.token_options = {
 			error_retry : this.MAX_ERROR_RETRY,
 			headers : {
@@ -133,12 +132,56 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 			}
 		};
 
+		var url = html.between(
+		// <meta name="viewer-api-url"
+		// content="/api/v1/viewer/stories/JXXtyieE41/50811.json">
+		'<meta name="viewer-api-url" content="', '"');
+		// console.log(url);
 		if (url) {
 			this.get_URL(url, callback, null, work_data.token_options);
 			return;
 		}
 
 		var chapter_data = work_data.chapter_list[chapter_NO - 1];
+
+		var url = html.between(
+				'<script id="__NEXT_DATA__" type="application/json">',
+				'</script>');
+		if (url) {
+			// 2020/6 via Google Chrome
+			Object.assign(chapter_data, JSON.parse(url));
+
+			var time = (new Date).format('%Y-%2m-%2dT%2H:%2M:%2S%z').replace(
+					/(\d{2})$/, ':$1');
+			if (false) {
+				var hash = crawler.CryptoJS.MD5(
+						time + chapter_data.runtimeConfig.salt).toString(
+						crawler.CryptoJS.enc.Hex);
+			}
+			var hash = crawler.forge.md.md5.create();
+			hash.update(time + chapter_data.runtimeConfig.salt);
+			hash = hash.digest().toHex();
+
+			// key: "getApiAppEpisodesIdReadRaw",
+			// https://comic.pixiv.net/_next/static/chunks/eb04f3552258e45f2446579a418399595863319c.e75f48a4015b43818882.js
+			// includes https://github.com/dmtrKovalenko/date-io
+			url = '/api/app/episodes/' + chapter_data.id + '/read';
+			// u.t.getApiAppEpisodesIdRead({xRequestedWith: u.z,
+			// https://comic.pixiv.net/_next/static/chunks/f76487c2815c6c0d6b3ac60de8a5a43085d6b6a4.38d2c49607f3f81dc8e8.js
+			this.setup_value('Referer', this.full_URL(this.chapter_URL(
+					work_data, chapter_NO)));
+			Object.assign(this.get_URL_options.headers, {
+				// Authorization : 'Bearer ' +
+				// this.configuration.accessToken("Bearer", []),
+
+				// "pixivcomic"
+				'X-Requested-With' : this.id,
+				'X-Client-Time' : time,
+				'X-Client-Hash' : hash
+			});
+			this.get_URL(url, callback, null, work_data.token_options);
+			return;
+		}
 
 		url = html.between('<meta name="token-api-url" content="',
 		// /api/v1/viewer/token/d220bbe0ed815ceea5fd0308021fff9f.json
@@ -169,20 +212,20 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 				if (html.error)
 					throw html.error;
 			} catch (e) {
-				if (_this.skip_error)
-					_this.onwarning(e);
+				if (this.skip_error)
+					this.onwarning(e);
 				else
-					_this.onerror(e);
+					this.onerror(e);
 				callback();
 				return;
 			}
 
 			chapter_data.token_data = html.data;
-			_this.get_URL('/api/v1/viewer/stories/'
+			this.get_URL('/api/v1/viewer/stories/'
 			// /api/v1/viewer/stories/___token___/00000.json
 			+ chapter_data.token_data.token + '/' + chapter_data.id + '.json',
 					callback, null, work_data.token_options);
-		}, {}, work_data.token_options);
+		}.bind(this), {}, work_data.token_options);
 	},
 	parse_chapter_data : function(html, work_data, get_label, chapter_NO) {
 		var chapter_data = work_data.chapter_list[chapter_NO - 1];
@@ -191,7 +234,7 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 			if (html.error)
 				throw html.error;
 		} catch (e) {
-			console.debug(html);
+			// console.debug(html);
 			if (typeof html === 'string' && html.includes('期限')) {
 				// e.g., html==="エピソードの公開期限が過ぎました"
 				chapter_data.limited = html;
@@ -204,21 +247,26 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 
 		// 避免覆寫 chapter_data.title
 		chapter_data = Object.assign(html.data, chapter_data);
-		html = html.data.contents;
-		if (html.length !== 1) {
-			throw '.length = ' + html.length;
-		}
-		// 設定必要的屬性。
-		chapter_data.image_list = [];
-		html[0].pages.forEach(function(view) {
-			for ( var page in view) {
-				if (false)
-					// 可行，但q=90會得到與q=50一樣的東西。
-					view[page].data.url = view[page].data.url.replace(
-							/q=[1-8]\d,/, 'q=90,')
-				chapter_data.image_list.push(view[page].data);
+		if (html.data.reading_episode) {
+			// 2020/6 via Google Chrome
+			chapter_data.image_list = html.data.reading_episode.pages;
+		} else {
+			html = html.data.contents;
+			if (html.length !== 1) {
+				throw new Error('.length = ' + html.length);
 			}
-		});
+			// 設定必要的屬性。
+			chapter_data.image_list = [];
+			html[0].pages.forEach(function(view) {
+				for ( var page in view) {
+					if (false)
+						// 可行，但q=90會得到與q=50一樣的東西。
+						view[page].data.url = view[page].data.url.replace(
+								/q=[1-8]\d,/, 'q=90,')
+					chapter_data.image_list.push(view[page].data);
+				}
+			});
+		}
 
 		return chapter_data;
 	},
@@ -237,4 +285,38 @@ var base_URL = 'https://comic.pixiv.net/', crawler = new CeL.work_crawler({
 
 // 缺少這個會從章節頁面跳回作品頁面。
 crawler.setup_value('cookie', 'open_work_page=yes');
-start_crawler(crawler, typeof module === 'object' && module);
+
+setup_crawler(crawler, typeof module === 'object' && module);
+
+if (false) {
+	CeL.get_URL_cache(
+	// https://cdnjs.com/libraries/crypto-js
+	'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js',
+			// https://cryptojs.gitbook.io/docs/
+			// https://github.com/brix/crypto-js
+			function(contents) {
+				crawler.CryptoJS = require(crawler.main_directory
+						+ 'crypto-js.min.js');
+				// console.log(crawler.CryptoJS.MD5('text').toString(crawler.CryptoJS.enc.Hex));
+				// start_crawler(crawler, typeof module === 'object' && module);
+			}, {
+				directory : crawler.main_directory
+			});
+}
+
+CeL.get_URL_cache(
+//
+'https://cdn.jsdelivr.net/npm/node-forge@0.7.0/dist/forge.min.js',
+// https://github.com/digitalbazaar/forge
+function(contents) {
+	crawler.forge = require(crawler.main_directory + 'forge.min.js');
+	if (false) {
+		var hash = crawler.forge.md.md5.create();
+		hash.update('text');
+		hash = hash.digest().toHex();
+		console.log(hash);
+	}
+	start_crawler(crawler, typeof module === 'object' && module);
+}, {
+	directory : crawler.main_directory
+});
