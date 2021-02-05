b1e16e64d8f4237a707d60b574a687302b3481c9
kanasimi <kanasimi@gmail.com>
2021-02-06 06:13:27 +0800

    fix #474: 扑飞漫画 改版. fix 將 epub 簡體中文電子書轉換為繁體中文的工具: try_LTP_server : true

--- book.cmn-Hant-TW/epub_hans_to_hant.js
+++ book.cmn-Hant-TW/epub_hans_to_hant.js
@@ -19,12 +19,18 @@ CeL.run([ 'application.storage.archive', 'application.storage.EPUB',
 'extension.zh_conversion' ], initialization);
 
 function initialization() {
+	return Promise.resolve(CeL.using_CeCC({
+		try_LTP_server : true
+	})).then(handle_files);
+}
+
+function handle_files() {
 	var epub_file_path = process.argv[2];
 	if (!epub_file_path) {
 		var main_script = require.main
 				&& require.main.filename.match(/[^\\\/]+$/)[0];
 		CeL.log('Convert epub: hans to hant. 將 epub 簡體中文電子書轉換為繁體中文的工具。\n\n'
-				+ 'Usage:\n	node ' + main_script + ' "epub.epub"');
+				+ 'Usage:\n	node ' + main_script + ' "path/to/epub.epub"');
 		return;
 	}
 
@@ -42,15 +48,31 @@ function initialization() {
 
 	CeL.debug('Extract epub files: ' + epub_file_path);
 	archive_file.ebook_file_list = [];
+
+	var convert_options = {
+		// only for debug CeCC 繁簡轉換。
+		cache_directory : CeL.append_path_separator(
+		//
+		epub_directory.replace(/[\/]*$/, ' - 繁簡轉換 cache')),
+		// 超過此長度才 cache。
+		min_cache_length : 20
+	};
 	process.stdout.write('Extracting ebook ' + epub_file_path + ' ...\r');
 	archive_file.extract({
 		output : epub_directory
-	}, function convert_files(output) {
+	}, convert_files);
+
+	function convert_files(output) {
 		// console.log(String(output));
 		// console.log(CeL.traverse_file_system + '');
-		CeL.traverse_file_system(epub_directory, for_text_file
-				.bind(archive_file));
+		return Promise.resolve().then(
+				CeL.traverse_file_system.bind(null, epub_directory,
+						for_text_file.bind(archive_file)))
+		//
+		.then(pack_up_files.bind(null, output));
+	}
 
+	function pack_up_files(output) {
 		archive_file.ebook_file_list = archive_file.ebook_file_list
 		// assert: path.startsWith(epub_directory+CeL.env.path_separator)
 		.map(function(path) {
@@ -71,23 +93,36 @@ function initialization() {
 		// TODO: if error occurred, do not remove directory.
 		CeL.debug('Remove directory: ' + epub_directory);
 		CeL.remove_directory(epub_directory, true);
-	});
-}
+		CeL.info('Convert epub: 繁簡轉換完畢: ' + converted_epub_file);
+	}
 
-function for_text_file(path, fso_status, is_directory) {
-	// 記錄原先已有的檔案。
-	if (path !== 'mimetype')
-		this.ebook_file_list.push(path);
+	function for_text_file(path, fso_status, is_directory, options) {
+		// 記錄原先已有的檔案。
+		if (path !== 'mimetype')
+			this.ebook_file_list.push(path);
 
-	if (!/\.(?:[sx]?html?|xml|te?xt|ncx|opf)$/i.test(path))
-		return;
+		if (!/\.(?:[sx]?html?|xml|te?xt|ncx|opf)$/i.test(path))
+			return;
 
-	process.stdout.write('Convert to hant: ' + path + ' ...\r');
-	var contents = CeL.get_file(path);
-	contents = CeL.CN_to_TW(contents)
-	// TODO: 把半形標點符號轉換為全形標點符號
-	.replace(/["'](?:zh-(?:cmn-)?|cmn-)?(?:Hans-)?CN["']/ig,
-	// "zh-TW"
-	'"zh-cmn-Hant-TW"');
-	CeL.write_file(path, contents);
+		process.stdout.write('Convert to hant ' + options.file_count + ': '
+				+ path + ' ...\r');
+		// CeL.info('for_text_file: Convert to hant: ' + path);
+		var contents = CeL.get_file(path);
+		return Promise.resolve().then(
+				CeL.CN_to_TW.bind(null, contents, convert_options))
+		//
+		.then(function(contents) {
+			contents = contents
+			// TODO: 把半形標點符號轉換為全形標點符號
+			.replace(/["'](?:zh-(?:cmn-)?|cmn-)?(?:Hans-)?CN["']/ig,
+			// "zh-TW"
+			'"zh-cmn-Hant-TW"');
+			if (false) {
+				CeL.info('for_text_file: Converted to hant: ' + path + ', '
+				//
+				+ contents.slice(0, 40) + '...');
+			}
+			CeL.write_file(path, contents);
+		});
+	}
 }
--- comic.cmn-Hans-CN/pufei.js
+++ comic.cmn-Hans-CN/pufei.js
@@ -16,10 +16,18 @@ var crawler = CeL.qTcms2014({
 	// 圖像檔案下載失敗處理方式：忽略/跳過圖像錯誤。當404圖像不存在、檔案過小，或是被偵測出非圖像(如不具有EOI)時，依舊強制儲存檔案。default:false
 	skip_error : true,
 
+	// 出錯：連線中斷
+	one_by_one : true,
+
+	// old: http://www.pufei.net/
 	// 2020/2/17 http://www.pufei8.com/
-	base_URL : 'http://www.pufei.net/',
+	base_URL : 'http://www.pufei8.com/',
 
 	postfix_image_url : function(url) {
+		// http://www.pufei8.com/manhua/32508/index.html
+		if (/^https?:\/\//.test(url))
+			return url;
+
 		// 2020/2/17 @see function loadview() @
 		// http://www.pufei.net/skin/2014mh/view.js
 		return 'http://res.img.fffimage.com/' + url;
