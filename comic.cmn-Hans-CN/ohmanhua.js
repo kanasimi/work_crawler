524b4a7616db06b221d45b61cdf893b1ef206d92
kanasimi <kanasimi@gmail.com>
2021-06-11 06:39:34 +0800

    fix #516: 提高oh漫畫特定圖源畫質

--- comic.cmn-Hans-CN/ohmanhua.js
+++ comic.cmn-Hans-CN/ohmanhua.js
@@ -271,6 +271,9 @@ var crawler = new CeL.work_crawler({
 					// @see __cr.switchWebp()
 					image_url += '.webp';
 				}
+				// e.g.,
+				// https://mhpic.xiaomingtaiji.net/comic/C/%E9%87%8D%E7%94%9F%E4%B9%8B%E5%89%91%E7%A5%9E%E5%BD%92%E6%9D%A5/%E7%AC%AC7%E8%AF%9DF0_295714/2.jpg-zymk.high.webp
+				image_url = image_url.replace('.middle.webp', '.high.webp');
 				chapter_data.image_list.push(image_url);
 			}
 		}
