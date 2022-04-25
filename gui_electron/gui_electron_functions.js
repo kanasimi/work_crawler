19934c8bae49e3a17868beaf315965eaea90ed4e
kanasimi <kanasimi@gmail.com>
2022-04-26 05:37:40 +0800

    + 介面語言

--- gui_electron/gui_electron_functions.js
+++ gui_electron/gui_electron_functions.js
@@ -441,7 +441,7 @@ function setup_language_menu() {
 
 	_.create_menu('language_menu',
 	// 預設介面語言繁體中文+...
-	[ 'TW', 'CN', 'ja', 'en', 'pt', 'fr', 'ko' ], on_menu_changed);
+	[ 'TW', 'CN', 'ja', 'en', 'fr', 'pt', 'pms', 'ko' ], on_menu_changed);
 
 	// translate all nodes to show in specified language (or default domain).
 	_.translate_nodes();
